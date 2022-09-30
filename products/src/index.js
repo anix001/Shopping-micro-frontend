50c4907fed23aa531bc3624d24bf83ca02ce0cec
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-09-30 14:38:32 +0545

    Improvement: Product micro frontend work complete

--- products/src/index.js
+++ products/src/index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+import faker from 'faker';
+
+let productString = "";
+
+  for (let i = 0; i < 5; i++) {
+    productString += `<div>${faker.commerce.productName()}</div>`;
+  }
+
+document.querySelector('#product-list').innerHTML = productString;
